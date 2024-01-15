37b1db3f560733875d974d1b9e4fd57d96dbc15a
YiShan <stephystar0228@gmail.com>
2024-01-15 22:00:40 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 分支 gh-pages
-git push -f https://github.com/YiShanStephyHung/yishan_space.git master:gh-pages
+git push -f https://github.com/YiShanStephyHung/yishan_space.git main:gh-pages
 
 cd -
