4a827e48efb385eafa6d95968901d18d5c6ee17e
YiShan <stephystar0228@gmail.com>
2024-01-16 21:37:35 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,39 @@
-#!/usr/bin/env sh
-# 當發生錯誤時終止腳本運行
+# #!/usr/bin/env sh
+# # 當發生錯誤時終止腳本運行
+# set -e
+
+# # 打包
+# npm run build
+
+# # 移動至到打包後的dist目錄 
+# cd dist
+
+# # 因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+# git init 
+# git add -A
+# git commit -m 'deploy'
+
+# # 部署到 分支 gh-pages
+# git push -f https://github.com/YiShanStephyHung/yishan_space.git gh-pages
+
+# cd -
+
+# test
 set -e
 
 # 打包
 npm run build
 
-# 移動至到打包後的dist目錄 
+# 若不是在main上
+# cd ..
+git add .
+git commit -m "update"
+git push origin main
+
+# 進到gh-pages
+# 需先刪除原gh-pages分支
 cd dist
-
-# 因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
-git init 
-git add -A
-git commit -m 'deploy'
-
-# 部署到 分支 gh-pages
-git push -f https://github.com/YiShanStephyHung/yishan_space.git main:gh-pages
-
-cd -
+git init
+git add .
+git commit -m "deploy"
+git push -u https://github.com/YiShanStephyHung/yishan_space.git gh-pages
--- src/router/index.js
+++ src/router/index.js
@@ -17,17 +17,9 @@ const routes = [
   }
 ]
 
-// const router = createRouter({
-//   mode: "hash",
-//   routes,
-// });
 const router = createRouter({
   history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes,
-// });
 
 export default router
