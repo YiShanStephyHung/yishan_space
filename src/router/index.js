57544957c22c65b4b76629adc776886d4dcb642c
YiShan <stephystar0228@gmail.com>
2024-01-16 21:26:23 +0800

    test2

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -17,8 +17,12 @@ const routes = [
   }
 ]
 
+// const router = createRouter({
+//   mode: "hash",
+//   routes,
+// });
 const router = createRouter({
-  mode: "hash",
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 // const router = createRouter({
